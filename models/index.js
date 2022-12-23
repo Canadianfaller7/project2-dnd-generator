1832db7e8c3a54973fb39a4c050d716634c7a34a
Spencer Merrill <canadianfaller7@gmail.com>
2022-12-22 19:07:45 -0700

    Created index.js for models to connect Models together

---
+++ models/index.js
@@ -0,0 +1,34 @@
+const User = require('./User');
+const Character = require('./Character');
+const Stats = require('./Stats');
+const Inventory = require('./Inventory');
+
+User.hasMany(Character, {
+  foreignKey: 'character_id',
+  onDelete: 'CASCADE'
+});
+
+Character.belongsTo(User, {
+  foreignKey: 'character_id'
+});
+
+Character.hasOne(Stats,  {
+  foreignKey: 'stats_id',
+  onDelete: 'CASCADE'
+});
+
+Stats.belongsTo(Character, {
+  foreignKey: 'stats_id'
+});
+
+Character.hasOne(Inventory, {
+  foreignKey: 'inventory_id',
+  onDelete: 'CASCADE'
+});
+
+Inventory.belongsTo(Character, {
+  foreignKey: 'inventory_id'
+});
+
+module.exports = { User, Character, Stats, Inventory };
+
