3b8c9bae3a5a9430c65f48d77224dc58afc797f0
Aaron Morgan <aaron.morgan.7@outlook.com>
2022-12-28 12:59:12 -0700

    added character routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 
-router.use('/', homeRoutes);
+//router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
