4f1720b876e93d3cdcdeaa507180fdad0ae23c2c
Spencer Merrill <canadianfaller7@gmail.com>
2023-01-02 19:29:49 -0700

    working to try and fix secret error for heroku deployment

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create();
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: process.env['SECRET'],
   cookie: {
     maxAge: 300000,
     httpOnly: true,
