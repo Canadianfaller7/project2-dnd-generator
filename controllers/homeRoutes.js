bdfc7da4d4d00391d622046b662b8410006c0d4c
Spencer Merrill <canadianfaller7@gmail.com>
2023-01-05 19:50:42 -0700

    added level to selected character and fixed logout issue

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 
     res.render('homepage', {
       characters,
-      logged_in: true
+      logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
